dcbbb76720932097cf4b96b125d4f7f8d347b8bb
Amirch85 <amirkumar2003@gmail.com>
2025-11-17 20:33:36 +0530

    add PM and AM and

--- script.js
+++ script.js
@@ -2,14 +2,24 @@ var hrs = document.querySelector('#hrs');
 var min = document.querySelector('#min');
 var sec = document.querySelector('#sec');
 var dayel = document.querySelector('#day');
+var h3 = document.querySelector('h3')
+
 
 var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 setInterval(() => {
     let currentime = new Date();
+  var boy;
+   if(currentime.getHours()>12){
+    boy = "pm";
+ }
+ else{
+       boy = "am"
+ }
 
+ 
     let hours = currentime.getHours() % 12;
-    hours = hours === 0 ? 12 : hours;
+    hours = hours == 0 ? 12 : hours;
 
     hrs.innerHTML = (hours < 10 ? '0' : '') + hours;
     min.innerHTML = (currentime.getMinutes() < 10 ? '0' : '') + currentime.getMinutes();
@@ -17,5 +27,6 @@ setInterval(() => {
 
     let day = currentime.getDay();
     dayel.innerHTML = weekdays[day];
+    h3.innerHTML = boy
 
 }, 500);
